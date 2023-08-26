2f6f6f61e28813a2fcc9a669d2b3e8eef2438834
Ernesti Komulainen <ernesti.komulainen@hotmail.com>
2023-08-26 14:35:46 +0300

    4.9 done

--- part4/bloglist/tests/blogsAPI.test.js
+++ part4/bloglist/tests/blogsAPI.test.js
@@ -6,6 +6,14 @@ const Blog = require('../models/blog')
 const api = supertest(app)
 
 describe('correct number of blogs', () => {
+
+    beforeAll(async () => {
+        await mongoose.connect(process.env.TEST_MONGODB_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        })
+      })
+    
     beforeEach(async () => {
         await Blog.deleteMany({})
 
@@ -33,6 +41,47 @@ describe('correct number of blogs', () => {
         expect(response.status).toBe(200)
         expect(response.type).toBe('application/json')
         expect(response.body).toHaveLength(2)
+    })
+
+    afterAll(async () => {
+        await mongoose.connection.close()
+    })
+})
+
+describe('correct id field', () => {
+
+    beforeAll(async () => {
+        await mongoose.connect(process.env.TEST_MONGODB_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        })
+      })
+      
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+
+        const initialBlogs = [
+            {
+                title: 'Blogi 1',
+                author: 'Kirjoittaja 1',
+                url: 'http://www.example.com/blog1',
+                likes: 10,
+            },
+            {
+                title: 'Blogi 2',
+                author: 'Kirjoittaja 2',
+                url: 'http://www.example.com/blog2',
+                likes: 5,
+            },
+        ]
+
+        await Blog.insertMany(initialBlogs)
+    })
+
+    test('id field is defined', async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body[0].id).toBeDefined()
     })
 
     afterAll(async () => {
