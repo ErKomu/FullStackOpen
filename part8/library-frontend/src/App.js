cc1e3b432cc8550079975f6cb037f9a75a5dccdc
Ernesti Komulainen <ernesti.komulainen@hotmail.com>
2023-10-28 14:35:27 +0300

    querys for books and authors

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -2,9 +2,35 @@ import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_BOOKS = gql`
+query {
+  allBooks {
+    title
+    author
+    published
+  }
+}
+`
+
+const ALL_AUTHORS = gql`
+query AllAuthors {
+  allAuthors {
+   name  
+   bookCount
+   born
+  }
+}
+`
 
 const App = () => {
   const [page, setPage] = useState('authors')
+
+  const allBooks = useQuery(ALL_BOOKS)
+  console.log(allBooks.data)
+  const allAuthors = useQuery(ALL_AUTHORS)
+  console.log(allAuthors.data)
 
   return (
     <div>
@@ -14,11 +40,12 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors show={page === 'authors'} />
+      <Authors authors={allAuthors.data} show={page === 'authors'} />
 
-      <Books show={page === 'books'} />
+      <Books books={allBooks.data} show={page === 'books'} />
 
       <NewBook show={page === 'add'} />
+
     </div>
   )
 }
